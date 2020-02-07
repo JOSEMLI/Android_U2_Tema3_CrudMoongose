e78e5ee9315759d778512c03bfc3a745dde35140
Jose Mamani <JOSEMLI@example.com>
2020-02-07 10:31:51 -0500

    ahora se iso el delete del producto que se puede visualizar en el logcat con el nombre deleteprod

--- app/src/main/java/com/example/android_u2_tema3_crudmoongose/servicesRetrofit.java
+++ app/src/main/java/com/example/android_u2_tema3_crudmoongose/servicesRetrofit.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
@@ -24,5 +25,9 @@ public interface servicesRetrofit {
   Call < Productos > getproducto(@Path("name") String name);
   @PUT("/products/{id}/update")
   Call<String> updateproducto(@Path("id") String id,@Body Productos producto);
+
+
+  @DELETE("/products/{id}/delete")
+  Call<String> deleteproducto(@Path("id") String id);
 
 }
